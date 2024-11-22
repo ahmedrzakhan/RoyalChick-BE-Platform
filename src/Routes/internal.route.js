24a3bc9f66508b958ce9c2ee965041e27bd238fa
Abubakar Amasa <amasaabubaakr@gmail.com>
2024-11-22 21:56:07 +0000

    trigger

--- src/Routes/internal.route.js
+++ src/Routes/internal.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { UserController } = require('../Controllers/user.controller');
-
+//
 const router = express.Router();
 
 router.get('/:id', UserController.getUser);
