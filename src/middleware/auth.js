26e3c1da897e4575d8323bd71525c2bbefee9fef
Ahmed Khan <khanahmed925@gmail.com>
2024-11-27 15:35:31 +0000

    verify iskitchen added

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -18,60 +18,81 @@ const verifyToken = async (req, res, next) => {
   }
 };
 
-
 const verifyTokenForEmployee = async (req, res, next) => {
-    try {
-        const authHeader = req.headers['authorization'];
-        const token = authHeader && authHeader.split('Bearer ')[1];
-        if (token == null) return res.status(401);
-        //verify token
-        const id = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        //verify from db
-        const employee = await EmployeeService.getEmployeeById(id.id);
-        if (!employee[0]) {
-          throw new Error('Employee not found');
-        }
-        req.user = employee[0];
-        next();
-    } catch (error) {
-        next(error);
-    }
-}
-//In addition, manager and exexcutive can perform staff actions
-const verifyUserTypeIsStaff = async (req, res, next) => {
   try {
-    if (req.user.position !== 'STAFF' && req.user.position!=='MANAGER' && req.user.position!='EXECUTIVE')         if (req.user.position !== 'EXECUTIVE') 
-        {
-        throw new Error('Forbidden');
-        }
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split('Bearer ')[1];
+    if (token == null) return res.status(401);
+    //verify token
+    const id = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    //verify from db
+    const employee = await EmployeeService.getEmployeeById(id.id);
+    if (!employee[0]) {
+      throw new Error('Employee not found');
+    }
+    req.user = employee[0];
     next();
   } catch (error) {
     next(error);
   }
-}
+};
+//In addition, manager and exexcutive can perform staff actions
+const verifyUserTypeIsStaff = async (req, res, next) => {
+  try {
+    if (
+      req.user.position !== 'STAFF' &&
+      req.user.position !== 'MANAGER' &&
+      req.user.position != 'EXECUTIVE'
+    )
+      if (req.user.position !== 'EXECUTIVE') {
+        throw new Error('Forbidden');
+      }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 const verifyUserTypeIsManager = async (req, res, next) => {
-    try {
-        if (req.user.position !== 'MANAGER'&&req.user.position!=='EXECUTIVE')         if (req.user.position !== 'EXECUTIVE') 
-            {
-            throw new Error('Forbidden');
-            }
-            next()
-    } catch (error) {
-        next(error);
+  try {
+    if (req.user.position !== 'MANAGER' && req.user.position !== 'EXECUTIVE')
+      if (req.user.position !== 'EXECUTIVE') {
+        throw new Error('Forbidden');
+      }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const verifyUserTypeIsKitchenStaff = async (req, res, next) => {
+  try {
+    if (req.user.position !== 'KITCHEN_STAFF') {
+      throw new Error('Forbidden');
     }
-    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 const verifyUserTypeIsExecutive = async (req, res, next) => {
-    try {
-        if (req.user.position !== 'EXECUTIVE') 
-            {
-            throw new Error('Forbidden');
-            }
-
-        next();
-    } catch (error) {
-        next(error);
+  try {
+    if (req.user.position !== 'EXECUTIVE') {
+      throw new Error('Forbidden');
     }
-}
-module.exports = { verifyToken, verifyUserTypeIsStaff, verifyUserTypeIsManager, verifyUserTypeIsExecutive, verifyTokenForEmployee };
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = {
+  verifyToken,
+  verifyUserTypeIsKitchenStaff,
+  verifyUserTypeIsStaff,
+  verifyUserTypeIsManager,
+  verifyUserTypeIsExecutive,
+  verifyTokenForEmployee,
+};
