549b3c596cab0207bcc62b9191f9cb992560b2e3
Ahmed Khan <khanahmed925@gmail.com>
2024-11-27 15:35:17 +0000

    middleware updated for internal

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userRoutes = require('./user.route');
 const internalRoutes = require('./internal.route');
 const { Autheticator } = require('../middleware/authenticate');
+const { verifyToken } = require('../middleware/auth');
 
 // Health check route
 router.get('/health', (req, res) => {
@@ -12,7 +13,7 @@ router.get('/health', (req, res) => {
 // Mount routes with their respective middleware
 router.use('/users', Autheticator.authenticateUser, userRoutes);
 
-router.use('/internal', Autheticator.authenticateInternal, internalRoutes);
+router.use('/internal', verifyToken, internalRoutes);
 
 const routerV1 = router;
 
